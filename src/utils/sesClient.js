047676eb92ef6530d1edf31c70cec4253001f7d5
Kamal Sharma <kamalsharmag123@gmail.com>
2025-11-04 18:23:01 +0530

    Remove sensitive data and use env variables

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -5,8 +5,8 @@ const REGION = "ap-south-1";
 const sesClient = new SESClient({
   region: REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: "AKIAYG24OPIEASEKQ55X",
+    secretAccessKey: "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",
   },
 });
 
