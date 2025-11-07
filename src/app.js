fe9b7a05416c3d36a76460064b2bb75f4c4b6e38
Kamal Sharma <kamalsharmag123@gmail.com>
2025-11-07 19:28:26 +0530

    feat: Chat implemented

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const connectDb = require("./config/database");
 const cors = require('cors');
 const app = express();
 const router = express.Router();
+const http = require("http");
 require('dotenv').config();
 
 app.use(cors({
@@ -18,91 +19,21 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const chatRouter = require("./routes/chat")
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
-
-// // get user by emailID
-// app.get("/user", async (req, res) => {
-//     const emailId = req.body.email;
-//     try {
-//         const user = await User.find({emailId: emailId});
-//         if(user.length === 0) {
-//             res.status(404).send("User not found");
-//         }
-//         res.send(user);
-//     } catch (err) {
-//         res.status(400).send("Something went wrong !!");
-//     }
-    
-// });
-
-// // get all user
-// app.get("/feed", async (req, res) => {
-//     try {
-//         const user = await User.find();
-//         if(user.length === 0) {
-//             res.status(404).send("User not found");
-//         }
-//         res.send(user);
-//     } catch (err) {
-//         res.status(400).send("Something went wrong !!");
-//     }
-    
-// });
-
-// // update user by id
-// app.patch("/user/:userId", async (req, res) => {
-//     const userId =req.params.userId;
-//     const data = req.body;
-//     try {
-//         const ALLOWED_UPDATE = [
-//             "firstName",
-//             "lastName",
-//             "password",
-//             "skills",
-//             "age",
-//             "gender"
-//         ];
-
-//         const isAllowedUpdate = Object.keys(data).every((k) => 
-//             ALLOWED_UPDATE.includes(k)
-//         )
-
-//         if(!isAllowedUpdate) {
-//             throw new Error("Update not allowed");
-//         }
-
-//         const user = await User.findByIdAndUpdate({_id: userId}, data, {
-//             returnDocument: "after",
-//             runValidators: true
-//         });
-//         res.send(user);
-//     }
-//     catch(err) {
-//         res.status(400).send("UPDATE FAILED: "+err.message);
-//     }
-// })
-
-// // delete user by id
-// app.delete("/user", async (req, res) => {
-//     const userId =req.body.userId;
-//     try {
-//         const user = await User.findOneAndDelete(userId);
-//         res.send(user);
-//     }
-//     catch(err) {
-//         res.status(400).send("something went wrong !!");
-//     }
-// })
-
+const httpServer = http.createServer(app);
+initializeSocket(httpServer);
 
 connectDb().then(() => {
     console.log("Database is connected");
-    app.listen(process.env.POST,() => {
+    httpServer.listen(process.env.POST,() => {
         console.log("SERVER STARTED");
     });
     
